3f29359339e164b6f341e8c7e76ad5593c69feb9
José Hydeo Mesquita Watase <peo_jwatase@uolinc.com>
2022-03-06 04:24:52 -0300

    backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,10 @@ import express, { response } from 'express';
 
 const app = express();
 
-
 app.get('/', (request, response) => {
-    return response.json({ message: "Upa Upa Papai" });
+  return response.json({ message: 'Upa Upa --- Hello GoStack' });
 });
 
 app.listen(3333, () => {
-    console.log("Listen on port 3333");
-})
+  console.log('Listen on port 3333');
+});
