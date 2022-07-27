565a17b68f04cf5a1927024add4dd0fa8549cc20
José Hydeo Mesquita Watase <peo_jwatase@uolinc.com>
2022-07-27 01:46:20 -0300

    Coding mail template provider.

---
+++ backend/src/shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO.ts
@@ -0,0 +1,9 @@
+interface IMailTemplateVariables {
+    [key: string]: string | number;
+}
+
+
+export default interface IParseMailTemplateDTO {
+    template: string;
+    variables: IMailTemplateVariables;
+}
