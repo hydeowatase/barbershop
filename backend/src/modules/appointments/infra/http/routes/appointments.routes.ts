cc14f5b79db8819457963d3dc0203e5658e0511b
Hydeo Watase <hydeowatase@gmail.com>
2022-08-09 19:06:52 -0300

    Implementing validations on routes.

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
@@ -11,7 +12,13 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post('/', celebrate({
+    [Segments.BODY]: {
+        provider_id: Joi.string().uuid().required(),
+        date: Joi.date().required(),
+    }
+}), appointmentsController.create);
+
 appointmentsRouter.get('/fromprovider', providerAppointmentsController.index);
 
 export default appointmentsRouter;
--- backend/src/modules/users/infra/http/routes/profile.routes.ts
+++ backend/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
@@ -9,8 +11,22 @@ const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
 
-profileRouter.put('/', profileController.update);
-profileRouter.get('/', profileController.show);
+profileRouter.get(
+    '/',
+    profileController.show);
+
+profileRouter.put(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            old_password: Joi.string(),
+            password: Joi.string(),
+            password_confirmation: Joi.string().valid(Joi.ref('password')),
+        }
+    }),
+    profileController.update);
 
 
 
